a8cf8d5b56fa15e048db04d3dc4bf66224f7e540
tanxcypro <58644969+tanxcypro@users.noreply.github.com>
2022-05-17 23:07:49 +0700

    Update conditionRendering.js

--- src/conditionRendering.js
+++ src/conditionRendering.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 
 //Create Component PrivatePage
+function PrivatePage(props) {
+  return(
+    <div>
+      <h1>hallo</h1>
+      <button onClick={props.logout}>logout</button>
+    </div>
+  )
+  
+}
 
 //Create Component GuestPage
+function GuestPage(props) {
+  return(
+    <div>
+      <h1>hallo</h1>
+      <button onClick={props.login}>login</button>
+    </div>
+  )
+  
+}
 
 function ConditionRenderin() {
   //init State
+  const [isLogin,setIsLogin]=useState(false)
+  console.log(isLogin);
   return (
     // Code Inside div
-    <div>{/* conditional logic rendering */}</div>
+    <div>{isLogin?(
+      <PrivatePage logout={()=>setIsLogin(!isLogin)}/>
+    ):(
+      <GuestPage login={()=>setIsLogin(!isLogin)}/>
+    )}</div>
   );
 }
 
